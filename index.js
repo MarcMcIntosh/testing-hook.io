2049afdda3b805e492b8b6fd7ac9d6bd8b320136
Marc McIntosh <marcmcintosh1987@gmail.com>
2019-05-15 17:52:54 +0200

    index.js should be ran by hook.io

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-
-/* Hook.params - Incoming HTTP request data ( query string and form fields )
-Hook.req
-Hook.res
-Hook.datastore
-Hook.fs
-Hook.sdk
-Hook.schema
-Hook.env
-Hook.streaming */
-
-
+const request = require('request')
 /* testing client sidewith
 fetch('https://hook.io/marc.mcintosh/testing-post-method', {
   method: 'POST',
   headers: { 'Content-type': 'application/json' },
-  body: JSON.stringify({ url: 'http://google.com' }),
+  body: JSON.stringify({ url: 'http://google.com', data: { message: 'hello' } }),
 }).then(console.log).catch(console.error)
 */
 module.exports = function handeler(hook) {
   const { req, res } = hook;
+  const { url, data } = req.body;
 
-  req.body.data
+  console.log(`${hook.time} [${req.method}]: from ${JSON.stringify(req.connection)}: for ${url}`);
+
+  if(req.method !== 'POST') {
+    res.satus(400)
+    return res.end();
+  }
+  request({
+    url,
+    method: 'POST',
+    body: data,
+    json: true,
+  }, (err, responce, body) => {
+    if(err) {
+      console.error(err);
+      res.send(err);
+    }
+    return res.json(response);
+  });
 }
