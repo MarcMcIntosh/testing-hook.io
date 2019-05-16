08954a398158ff2a0a7daaba97f3271805d18e2e
Marc McIntosh <marcmcintosh1987@gmail.com>
2019-05-16 13:48:52 +0200

    First test passes, spies not working as inteneded, but not required.

--- __tests__/dropRequestsFor.spec.js
+++ __tests__/dropRequestsFor.spec.js
@@ -4,17 +4,15 @@ describe('src/dropRequestsFor', () => {
 
   it('it returns a function that rejects hooks with a matching ip', () => {
     const local = dropRequestsFor('127.0.0.1');
+
     const hook = {
-      req: {
-        body: {
-          url: '127.0.0.1',
-        }
-      },
-      res: {
-        status: jest.fn(),
-        end: jest.fn()
-      }
+      req: { body: { url: '127.0.0.1' } },
     };
+
+    dropRequestsFor(hook, (err, res) => {
+      expect(err).toBe(true);
+    });
+
   });
 
   /* it('p[erforms a dns look up on the payload to find the ip address', () => {
@@ -22,7 +20,7 @@ describe('src/dropRequestsFor', () => {
     const hook = {
       req: {
         body: {
-          url: '127.0.0.1',
+          url: 'localhost',
         }
       },
       res: {
--- src/dropRequestsFor.js
+++ src/dropRequestsFor.js
@@ -1,11 +1,16 @@
 
-const dropRequestsFor = (address) => (hook) => {
+const dropRequestsFor = address => (hook, callback) => {
   const { req: { body: { url }}} = hook;
 
   if (address === url) {
-    res.status(403);
-    res.send(`Refussing to send hook to ${url}`);
+
+    console.log('Rejectng request for ' + JSON.stringify(hook.req));
+    return callback(true, hook)
+    // hook.res.status(403);
+    // hook.res.send(`Refussing to send hook to ${url}`);
+
   }
+  
 }
 
 module.exports = dropRequestsFor;
